5107c86c452333f3855811cdce4cfa98c9ffbd0b
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2022-02-16 09:51:19 -0300

    init start recyclerVIew list data mock

---
+++ app/src/main/java/com/example/rampupjfl/data/DataSource.kt
@@ -0,0 +1,23 @@
+package com.example.rampupjfl.data
+
+import com.example.rampupjfl.R
+import com.example.rampupjfl.model.Affirmation
+
+class DataSource {
+
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
