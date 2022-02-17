3edf735a493fe174636c4fa4ba69b8d613a4f94b
Vinicius Silva <vinicius.silva@virtus.ufcg.edu.br>
2022-02-17 09:42:27 -0300

    add float action button

--- app/src/main/java/com/example/rampupjfl/MainActivity.kt
+++ app/src/main/java/com/example/rampupjfl/MainActivity.kt
@@ -8,12 +8,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.rampupjfl.adapter.ItemAdapter
 import com.example.rampupjfl.data.DataSource
 import com.example.rampupjfl.model.Item
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     private val myDataSet = DataSource().generateDummyList(10)
     private val adapter = ItemAdapter(this, myDataSet)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +29,12 @@ class MainActivity : AppCompatActivity() {
         // Use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true)
-
+        val fab: View = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Item add at position", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            insertItem(fab)
+        }
     }
 
     fun insertItem(view: View) {
